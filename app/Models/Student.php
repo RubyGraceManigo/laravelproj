c819d8c540956671f1407bf75765c2666340b12f
RubyGraceManigo <manigorubygrace@gmail.com>
2023-10-03 15:38:01 -1200

    added activity # 7

---
+++ app/Models/Student.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'students';
+
+    //to retrive all students
+    $student = \App\Models\Students::all();
+
+
+    //create new student record
+    $student = new \App\Models\Students();
+    $student->name = 'John Doe';
+    $student->email = 'john@example.com';
+    $student->save();
+
+    //update student record
+    $student\App\Model\Student::find(i); //assuming the student with 1 exists
+    $student->name = 'Updated Name';
+    $student->save();
+
+    //delete student record
+    $student = \App\Models\Student::find(i); //assuming the student with 1 exists
+    $student->delete();
+
+    use HasFactory;
+}
